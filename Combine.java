5bacc72c0f0400c5605ebb00b8adb864fb982e8a
kishor2581 <Privatework278@gmail.com>
2025-04-08 17:06:44 +0530

    Save to master branch

--- Combine.java
+++ Combine.java
@@ -1,5 +1,6 @@
 public class Combine {
     public static void main(String[] args) {
         System.out.println("This is Devops lab");
+        System.out.println("This is Devops Lab Master");
     }
 }
